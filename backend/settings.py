3e3503b8c67cebb9acc99bca60fb38bcae50ec19
seejn <seejnmaharjan@gmail.com>
2024-05-29 11:45:52 +0545

    created roles models

--- backend/settings.py
+++ backend/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = 'Cusers.CustomUser'
 
 # Application definition
 
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "Cusers",
+    "Roles",
 ]
 
 MIDDLEWARE = [
