ece7a0e677b002a5081f39f1adf0515528b662ea
ramila1 <ramilashahi2799@gmail.com>
2024-05-29 11:34:04 +0545

    track changed

--- track/models.py
+++ track/models.py
@@ -1,14 +1,22 @@
 from django.db import models
-import CustomUser
+from CustomUser.models import CustomUser
+from django.utils import timezone
+from genre.models import Genre
 
-class Album(models.Model):
-    title= models.CharField(max_length=20,unique=True)
-    artist=models.ForeignKey(CustomUser,on_delete=models.CASCADE)
-    released_date=models.DateField(null=True)
-    image=models.CharField(default="image.png")
-
-    class Meta:
-        db_table = "album"
+class Music(models.Model):
+    title = models.CharField(max_length=100)
+    duration = models.CharField(max_length=10)
+    artist = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    release_date = models.DateField(auto_now_add=True)
+    is_deleted=models.BooleanField(default=False)
+    cover = models.CharField(default="image.png")  
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    
 
     def __str__(self):
-        return "album"
+        return self.title
+
+    def soft_delete(self):
+        self.is_deleted = True
+        self.deleted_at = timezone.now()
+        self.save()
