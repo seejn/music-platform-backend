27f2074f2e61d539540b721c47fc3d523922d191
seejn <seejnmaharjan@gmail.com>
2024-05-30 12:05:15 +0545

    added serializer for track

---
+++ track/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Music
+
+from Cusers.serializers import ArtistSerializer
+from genre.serializers import GenreSerializer
+
+class TrackSerializer(serializers.ModelSerializer):
+    artist = ArtistSerializer(read_only=True)
+    genre = GenreSerializer(read_only=True)
+    class Meta:
+        model = Music
+        fields = "__all__"
+
+    
