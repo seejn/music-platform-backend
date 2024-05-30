400d20dae854115b68aa85915c1ab31d608a531a
seejn <seejnmaharjan@gmail.com>
2024-05-30 22:19:38 +0545

    modified response to json on ObjectNotFound

--- Roles/views.py
+++ Roles/views.py
@@ -9,19 +9,23 @@ from Cusers.models import CustomUser
 
 
 def get_all_artist(request):
-        artist_role = Role.objects.get(pk=2)
-        all_artist = artist_role.user.all()
-        serializer = ArtistSerializer(all_artist, many=True)
-        return JsonResponse(serializer.data, safe=False)
+    artist_role = Role.objects.get(pk=2)
+    all_artist = artist_role.user.all()
+    
+    if not all_artist:
+        return JsonResponse({"message": "No artists available"}, status=404)
+
+    serializer = ArtistSerializer(all_artist, many=True)
+    return JsonResponse(serializer.data, status=200)
 
 
 
 def get_current_artist(request, pk):
-        try:
-            artist = CustomUser.objects.get(id=pk)
-        except CustomUser.DoesNotExist:
-            raise NotFound(detail="Artist not found")
+    try:
+        artist = CustomUser.objects.get(id=pk)
+    except CustomUser.DoesNotExist:
+        return JsonResponse({"message": "Artist not available"}, status=404)
 
-        serializer = ArtistSerializer(artist)
-        return JsonResponse(serializer.data, status=status.HTTP_200_OK)
+    serializer = ArtistSerializer(artist)
+    return JsonResponse(serializer.data, status=200)
 
