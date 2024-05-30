3942f5270d0492482d488abf3940c5f1630393a6
seejn <seejnmaharjan@gmail.com>
2024-05-30 21:55:17 +0545

    fixed typo

--- Roles/urls.py
+++ Roles/urls.py
@@ -3,5 +3,5 @@ from .views import get_all_artist,get_current_artist
 
 urlpatterns = [
     path('artists/', get_all_artist, name='get_all_artist'),
-    path('artists/<int:pk>/', get_current_artist, name='current_artist_detail'),
+    path('artist/<int:pk>/', get_current_artist, name='current_artist_detail'),
 ]
