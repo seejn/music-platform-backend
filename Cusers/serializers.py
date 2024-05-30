52f09bd47e26503cc6b52e430beab285ce0ca9cb
seejn <seejnmaharjan@gmail.com>
2024-05-30 10:30:42 +0545

    removed role from artistSerializer

--- Cusers/serializers.py
+++ Cusers/serializers.py
@@ -4,20 +4,19 @@ from .models import CustomUser, ArtistDetail
 from Roles.models import Role
 from Roles.serializers import RoleSerializer
 
-class CustomUserSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = CustomUser
-        fields = ['id', 'email', 'image', 'role', 'is_deleted']
-
 class ArtistDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = ArtistDetail
         fields = '__all__'
 
+class CustomUserSerializer(serializers.ModelSerializer):
+    role = RoleSerializer(read_only=True)
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'email', 'image', 'role', 'is_deleted']
+
 class ArtistSerializer(serializers.ModelSerializer):
     detail = ArtistDetailSerializer(source="artistdetail", read_only=True)
-    role = RoleSerializer(read_only=True)
     class Meta:
         model = CustomUser
         exclude = [
