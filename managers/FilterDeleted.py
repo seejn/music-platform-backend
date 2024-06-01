b4156e0be4805c57a10e8515deceb15d2df62fa1
seejn <seejnmaharjan@gmail.com>
2024-06-01 16:15:25 +0545

    modified FilterDeletedManager -> SoftDeleteManager

--- managers/FilterDeleted.py
+++ managers/FilterDeleted.py
@@ -1,5 +1,5 @@
 from django.db import models
 
-class FilterDeletedManager(models.Manager):
+class SoftDeleteManager(models.Manager):
     def get_queryset(self):
         return super().get_queryset().filter(is_deleted=False)
