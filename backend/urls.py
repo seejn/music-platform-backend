e7a6b9d34505f7ae29421ca1ac5d1ddfbf1fcae4
seejn <seejnmaharjan@gmail.com>
2024-06-03 21:55:50 +0545

    configuration for image upload

--- backend/urls.py
+++ backend/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     # path("track/",include('track.urls')),
@@ -25,4 +28,4 @@ urlpatterns = [
     path("users/",include("Cusers.urls")),
     path("roles/",include("Roles.urls")),
     path("track/",include("track.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
