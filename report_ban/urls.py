df9dfb19a49d4ff6fed4513cc953e7466ccf28c1
dyo78 <maharjan.diyaa@gmail.com>
2024-06-09 23:06:08 +0545

    made urls and fixed name of function reportedtrackofartist

--- report_ban/urls.py
+++ report_ban/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('report_track/<int:track_id>/', views.report_track, name="report_track"),
+    path('get_all_reported_tracks/', views.get_all_reported_tracks, name="get_all_reported_tracks"),
+    path('get_reported_tracks_of_artist/<int:artist_id>/', views.get_reported_tracks_of_artist, name="get_reported_tracks_of_artist"),
+    path('get_all_banned_tracks/', views.get_all_banned_tracks, name="get_all_banned_tracks"),
+    path('get_banned_tracks_of_artist/<int:artist_id>/', views.get_banned_tracks_of_artist, name="get_banned_tracks_of_artist"),
+
 ]
