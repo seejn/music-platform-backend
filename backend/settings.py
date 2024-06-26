e1902b530c22f311b6bcb735793fa982d4de712a
seejn <seejnmaharjan@gmail.com>
2024-06-26 16:00:39 +0545

    SECRET_KEY moved to .env
    added Debug login

--- backend/settings.py
+++ backend/settings.py
@@ -22,10 +22,10 @@ load_dotenv(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-x9b5!++4hzaf$ei77ciu+zg8lz$7yet4atfxnk2u3$@8&1y7z&"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG')
+DEBUG = os.getenv('DEBUG') == "True"
 
 ALLOWED_HOSTS = [host.strip() for host in os.getenv('ALLOWED_HOSTS').split(',')]
 
